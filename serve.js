a2ba8e8984fdfb33e1b7a6e50d0eebc90d85aef1
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-04-18 11:55:23 +0530

    Update serve.js

--- serve.js
+++ serve.js
@@ -14,7 +14,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,DELETE,PUT",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(session({
     secret: "your_session_secret",
