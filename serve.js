84e73cc8f108063d01d86a39f13b90436936d4f1
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-04-18 11:38:46 +0530

    sdf
    sd

--- serve.js
+++ serve.js
@@ -11,7 +11,7 @@ require("./config/passport-setup");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
